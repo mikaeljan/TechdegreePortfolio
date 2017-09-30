809e2d9f92fb970fec5126f1c48684a1620cb104
Michal Janek <mikael.janek@gmail.com>
2017-10-01 00:36:41 +0200

    fix slick feature

--- src/js/main.js
+++ src/js/main.js
@@ -19,8 +19,15 @@ $(document).ready(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
     });
+    // $('#portfolio').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 4000,
+    // });
+
 
     //End carousel scrolling
 });
